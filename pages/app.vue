2c45079dc03eccce0a98fa5de0e5b816671a3eed
Taisei Toya <tytaisei0102@gmail.com>
2021-04-19 10:16:37 +0200

    feat(app.vue, auth.ts): 認証用URLを実装

---
+++ pages/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <v-btn>
+        {{authRequestUri}}
+      </v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Vue, Component} from 'vue-property-decorator'
+import {authRequestUri} from '@/plugins/auth'
+
+@Component
+export default class TestComponent extends Vue {
+  private authRequestUri : string = authRequestUri;
+}
+</script>
