5ddd7962af53ac5791f2ffd2e84b79738abf22e1
Taisei Toya <tytaisei0102@gmail.com>
2021-04-26 10:16:12 +0200

    feat(UserProfileCard): Login後、ユーザー情報を表示する機能を追加

---
+++ components/UserProfileCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card>
+    <v-row>
+      <v-col cols="6">
+        <v-img :src="userProfile.image"></v-img>
+      </v-col>
+      <v-col>
+        <v-row>
+          <v-col>UserName：{{ userProfile.display_name }}</v-col>
+        </v-row>
+        <v-row>
+          <v-col> Country: {{ userProfile.country }} </v-col>
+        </v-row>
+        <v-row
+          ><v-col>Email: {{ userProfile.email }}</v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+import UserProfile from '@/middleware/fetchUserProfile'
+
+@Component
+export default class UserProfileCard extends Vue {
+  @Prop()
+  private userProfile!: UserProfile
+}
+</script>
--- pages/app.vue
+++ pages/app.vue
@@ -1,41 +1,47 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <a :href="authUrl">{{ authUrl }}</a>
-      <h1>{{ token }}</h1>
-      <h2>{{ userProfile }}</h2>
-    </v-col>
-  </v-row>
+  <div>
+    <v-row>
+      <v-col class="text-center">
+        <v-btn :href="authUrl" rounded>Login with Spotify</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <user-profile-card :userProfile="userProfile"> </user-profile-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
 import { userInfoStore } from '@/store'
 import { Context } from '@nuxt/types'
-import UserProfile, { fetchUserProfile } from '@/middleware/fetchUserProfile'
+import { fetchUserProfile } from '@/middleware/fetchUserProfile'
+import UserProfileCard from '@/components/UserProfileCard.vue'
 
-@Component
+@Component({
+  components: {
+    UserProfileCard,
+  },
+})
 export default class AuthApp extends Vue {
   get token(): string {
     return userInfoStore.getToken
   }
 
-  get authUrl() {
+  async asyncData(context: Context): Promise<{}> {
+    const { $config } = context
     const url = new URL('https://accounts.spotify.com/authorize')
     const scopes: string[] = ['user-read-private', 'user-read-email']
-
     url.searchParams.set('response_type', 'code')
     url.searchParams.set('scope', scopes.join(' '))
-    url.searchParams.set('redirect_uri', this.$config.redirectUri)
-    url.searchParams.set('client_id', this.$config.clientId)
+    url.searchParams.set('redirect_uri', $config.redirectUri)
+    url.searchParams.set('client_id', $config.clientId)
     url.searchParams.set('state', 'state')
-    return url.href
-  }
-
-  async asyncData(context: Context): Promise<{}> {
-    // const {} = context
+    const authUrlRes = url.href
     const userProfileRes = await fetchUserProfile()
-    return { userProfile: userProfileRes }
+    return { userProfile: userProfileRes, authUrl: authUrlRes }
   }
 }
 </script>
