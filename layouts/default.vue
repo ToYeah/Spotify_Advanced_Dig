dc340579d41223dc5358d84aa0f6d56660355e8e
Taisei Toya <tytaisei0102@gmail.com>
2021-05-09 10:36:56 +0200

    feat(app-bar): ボタンにリンクを追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,14 @@
     <v-app-bar app color="primary">
       <h1 class="font-weight-bold white--text subtitle-1">AdvancedDig</h1>
       <v-spacer></v-spacer>
-      <v-btn x-large icon color="white">
+      <v-btn
+        x-large
+        icon
+        color="white"
+        href="https://github.com/ToYeah/spotify_search"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <v-icon>mdi-github</v-icon>
       </v-btn>
     </v-app-bar>
