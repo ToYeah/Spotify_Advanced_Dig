bd89a9c92313e387b5137b5699f6c9733bd4b0e9
Taisei Toya <tytaisei0102@gmail.com>
2021-05-09 09:51:32 +0200

    feat(OptionSlider): 各プロパティに詳細情報を追加

--- components/OptionSlider.vue
+++ components/OptionSlider.vue
@@ -4,6 +4,49 @@
       <v-col class="pb-1">
         <div class="grey--text text--darken-1 subtitle-1">
           {{ unit.name }}
+
+          <v-dialog v-model="dialog" width="500">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color="primary"
+                icon
+                x-small
+                :href="trackUri"
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon>mdi-information-outline</v-icon>
+              </v-btn>
+            </template>
+
+            <v-card>
+              <v-card-title class="headline grey lighten-2">
+                {{ unit.name }}
+              </v-card-title>
+
+              <v-card-text>
+                {{ information }}
+              </v-card-text>
+
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="primary"
+                  text
+                  :href="reference"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  reference
+                </v-btn>
+                <v-btn color="primary" text @click="dialog = false">
+                  CLOSE
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </div>
       </v-col>
     </v-row>
@@ -24,13 +67,19 @@
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'nuxt-property-decorator'
-import SearchOptionUnit from '@/middleware/SearchOptionUnit'
+import SearchOptionUnit, {
+  unitInformations,
+} from '@/middleware/SearchOptionUnit'
 
 @Component
 export default class OptionSlider extends Vue {
+  private reference =
+    'https://developer.spotify.com/documentation/web-api/reference/'
+  private dialog = false
+
   @Prop()
   private unit!: SearchOptionUnit
-  @Prop()
-  private propertyName!: string
+
+  private information = unitInformations[this.unit.name]
 }
 </script>
--- middleware/SearchOptionUnit.ts
+++ middleware/SearchOptionUnit.ts
@@ -6,3 +6,20 @@ export default class SearchOptionUnit {
     public readonly range: number[]
   ) {}
 }
+
+interface InfoObject {
+  [key: string]: any
+}
+
+export const unitInformations: InfoObject = {
+  Danceability:
+    'Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable.',
+  Energy:
+    'Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on the scale. Perceptual features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy.',
+  Popularity:
+    'The popularity of the track. The value will be between 0 and 100, with 100 being the most popular. The popularity of a track is a value between 0 and 100, with 100 being the most popular. The popularity is calculated by algorithm and is based, in the most part, on the total number of plays the track has had and how recent those plays are. Generally speaking, songs that are being played a lot now will have a higher popularity than songs that were played a lot in the past. Duplicate tracks (e.g. the same track from a single and an album) are rated independently. Artist and album popularity is derived mathematically from track popularity. Note that the popularity value may lag actual popularity by a few days: the value is not updated in real time.',
+  Valence:
+    'A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry).',
+  Instrumentalness:
+    'Predicts whether a track contains no vocals. “Ooh” and “aah” sounds are treated as instrumental in this context. Rap or spoken word tracks are clearly “vocal”. The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content. Values above 0.5 are intended to represent instrumental tracks, but confidence is higher as the value approaches 1.0.',
+}
